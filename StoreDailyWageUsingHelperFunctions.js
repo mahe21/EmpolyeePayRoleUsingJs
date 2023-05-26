0a2574f5868f748fc08ea85b16b793f6cb0fe2a1
Mahesh <Maheshk2104@gmail.com>
2023-05-26 18:04:37 +0530

    UC7

---
+++ StoreDailyWageUsingHelperFunctions.js
@@ -0,0 +1,51 @@
+function calcDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const IS_PART_TIME_HOURS = 4;
+const IS_FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NO_OF_WORKING_DAYS = 20;
+let empHrs = 0;
+const Max_Hrs_In_Month = 160;
+const Num_Of_Working_Days = 20;
+let totalEmpHours = 0;
+let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+
+
+function getWorkingHours(empCheck) 
+{
+    switch(empCheck)
+    {
+        case IS_PART_TIME:
+            return IS_PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return IS_FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+while (totalEmpHours<= Max_Hrs_In_Month && totalWorkingDays < Num_Of_Working_Days) {
+    totalWorkingDays++; 
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHours += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
+} 
+let empWage = calcDailyWage(totalEmpHours);
+
+let totEmpWage = 0;
+function sum(dailyWage) 
+{
+    totEmpWage += dailyWage;
+}
+empDailyWageArr.forEach(sum);
+console.log("UC6 - Total Days: " + totalWorkingDays + "  Total Hrs: " + totalEmpHours + "  Emp Wage: " + totEmpWage);
+
+function totalWages(totalWage, dailyWage)
+{
+    return totalWage + dailyWage;
+}
+console.log("Emp Wage With Reduce:" + empDailyWageArr.reduce(totalWages, 0));
