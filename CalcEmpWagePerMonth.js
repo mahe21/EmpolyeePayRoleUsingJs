9bee6bd66abd6b448cc9c5f777df87acf8fa0a59
Mahesh <Maheshk2104@gmail.com>
2023-05-26 15:56:29 +0530

    UC4

---
+++ CalcEmpWagePerMonth.js
@@ -0,0 +1,28 @@
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const IS_PART_TIME_HOURS = 4;
+const IS_FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NO_OF_WORKING_DAYS = 20;
+let empHrs = 0;
+
+function GetWorkingHours(empCheck) 
+{
+    switch(empCheck)
+    {
+        case IS_PART_TIME:
+            return IS_PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return IS_FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+
+for(let Day = 0; Day <= NO_OF_WORKING_DAYS; Day++) 
+{
+    empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += GetWorkingHours(empCheck);
+}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Total Hours: " + empHrs + "  Total Wage: " + empWage);
