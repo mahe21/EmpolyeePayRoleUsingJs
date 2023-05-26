de8d57bcf0c9581b5a2a1adb822b8a402b7544ab
Mahesh <Maheshk2104@gmail.com>
2023-05-26 15:55:24 +0530

    UC3

---
+++ CalculatingEmployeeWageUsingFunction.js
@@ -0,0 +1,22 @@
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const IS_PART_TIME_HOURS = 4;
+const IS_FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+function GetWorkingHours(empCheck) {
+    switch(empCheck)
+    {
+        case IS_PART_TIME:
+            return IS_PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return IS_FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = GetWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage: " + empWage);
